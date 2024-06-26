5682341aa67020b5446545b0bdf27fa09c973d0d
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2024-06-26 18:33:27 +0200

    Added Recipe Class

---
+++ Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sashiel_ST10028058_PROG6221_POE
+{
+    internal class Recipe
+    {
+    }
+}
